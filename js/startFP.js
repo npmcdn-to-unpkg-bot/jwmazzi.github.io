976a4c31c86dc892500c94a9b022ff37898c22f7
jwmazzi <jeffreyscarmazzi@gmail.com>
2016-07-23 10:36:06 -0400

    leaf

--- js/startFP.js
+++ js/startFP.js
@@ -1,3 +1,33 @@
 $(document).ready(function() {
 			    $('#fullpage').fullpage();
 			});
+
+
+var moveOneMap = L.map('map').setView([51.505, -123], 13);
+
+
+
+
+var CartoDB_DarkMatterNoLabels = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+	subdomains: 'abcd',
+	maxZoom: 19
+});
+
+var Thunderforest_Transport = L.tileLayer('http://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://www.opencyclemap.org">OpenCycleMap</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	maxZoom: 19
+});
+
+
+var baseMaps = {
+	"Transport": Thunderforest_Transport,
+	"Dark": CartoDB_DarkMatterNoLabels
+};
+
+
+var map = L.map('map', {
+	layers: [Thunderforest_Transport]
+	}).setView([38.37652, -75.63606], 13);
+
+L.control.layers(baseMaps, overLays).addTo(map);
